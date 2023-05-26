27aa80a13b6fdba2dfa1e122d2afaac32d6cb603
ridvanmsl <93757575+ridvanmsl@users.noreply.github.com>
2023-05-26 18:37:01 +0300

    initial push

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,37 @@
+// B201202062 - RIDVAN MUSAOĞLU
+// SOFTWARE VERIFICATION AND VALIDATION HOMEWORK 1
+//
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10, 2, 5",
+            "10, 4, 2.5",
+            "12.5, 5, 2.5",
+            "10, 2.5, 4",
+            "12.5, 2.5, 5"
+    })
+    void testDivision(double a, double b, double expected) {
+        assertEquals(expected, Calculator.divide(a, b));
+    }
+
+}
